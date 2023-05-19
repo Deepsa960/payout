39a713d8d3b14794863afadb5dd646de7ebb3674
Chandan Gaur <chandangaurjecrc@gmail.com>
2023-05-19 15:43:19 +0530

    login ui updated2

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import Admin from "./Component/Admin";
 function App() {
   return (
     <div className="App">
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/ForgotPassword" element={<ForgotPassword />} />
-        <Route path="/Otp" element={<Otp/>} />
-        <Route path="/Admin" element={<Admin/>} />
+        <Route path="/Otp" element={<Otp />} />
+        <Route path="/Admin" element={<Admin />} />
       </Routes>
     </div>
   );
